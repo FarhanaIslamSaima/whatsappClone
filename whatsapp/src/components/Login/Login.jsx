41e3f0b7ba4a9f5f4cc229a85127078bae1b7072
FarhanaIslamSaima <farhanaislamsaim@gmail.com>
2022-11-11 20:53:37 +0600

    updated

--- whatsapp/src/components/Login/Login.jsx
+++ whatsapp/src/components/Login/Login.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import {withStyles,Dialog,makeStyles,Box,Typography,AppBar,Toolbar} from '@material-ui/core'
+import pic from '../assets/code.png'
 const style={
     DialogPaper:{
         minWidth:'1000px',
@@ -9,20 +10,30 @@ const style={
 }
 const useStyle=makeStyles({
   container:{
-    display:'flex'
+    display:'flex',
+    padding:'40px',
+    
     
 
   },
   leftContainer:{
-     width:'60%'
+     width:'80%'
   },
   rightContainer:{
     width:"40%"
 
   },
+  desc:{
+      display:'flex',
+      flexDirection:'column',
+      marginTop:'20px',
+'&>*':{
+  padding:'10px'
+}
+  },
   header:{
   height:'300px',
-    backgroundColor:'green'
+    backgroundColor:'#128C7E'
   }
 
 })
@@ -41,12 +52,25 @@ const Login = ({classes}) => {
  
            <Dialog open={true} classes={{paper:classes.DialogPaper}}>
       <Box className={useClass.container}>
+  
        <Box className={useClass.leftContainer}>
-        <Typography variant={'h4'}>To use Whatsapp On your Computer</Typography>
+       <Typography variant={'h4'}>To use Whatsapp On your Computer</Typography>
+       <Box className={useClass.desc}>
+        <Typography>1.Open Whatsapp One your phone</Typography>
+        <Typography>2.Tap Menu : or settings and select whatsapp web</Typography>
+        <Typography>3.point your phone on the screen to capture code</Typography>
+        
+
+       </Box>
+      
 
        </Box>
        <Box className={useClass.rightContainer}>
+        <Box className={useClass.displayImage}>
+        <img src={pic} className={useClass.image}/>
 
+        </Box>
+      
          </Box>
 
       </Box>
