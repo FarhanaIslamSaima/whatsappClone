e1fe37aaf4c734c5a8af38ca9c35eb86b52e55aa
FarhanaIslamSaima <farhanaislamsaim@gmail.com>
2022-12-04 14:19:49 +0600

    updated

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -1,6 +1,20 @@
+import UserModel from "../model/UserModel.js";
+
+
 export const addUser=async(req,response)=>{
     try{
-       response.status(200).json(req.body);
+        let exist=UserModel.findOne({googleId:req.body.googleId});
+        if(exist){
+            response.status(200).josn("User already exist")
+        }
+        else{
+            const newUser=new UserModel(req.body);
+            newUser.save();
+           response.status(200).json(req.body);
+           console.log(req.body);
+
+        }
+    
     }
     catch(error){
         response.status(500).json(error);
---
+++ server/model/UserModel.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+
+const UserSchema=new mongoose.Schema({
+    googleId:{
+        type:String
+    },
+    image:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true
+    },
+    name:{
+        type:String,
+        require:true
+    },
+    givenName:{
+        type:String,
+        require:true
+    },
+    familyName:{
+        type:String,
+        require:true
+
+    }
+
+})
+const UserModel=mongoose.model('UserModel',UserSchema)
+export default UserModel
